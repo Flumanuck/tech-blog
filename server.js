eb413476bf862b6c60d8d61275dc586dd75165a5
Flumanuck <55293870+Flumanuck@users.noreply.github.com>
2021-11-13 23:47:49 -0500

    added handlebar bases+routes

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 // Dependencies
 // =============================================================
 const express = require("express");
-// const exphbs = require("express-handlebars");
+const { engine } = require("express-handlebars");
 // Requires the 'express-session' module
-// const session = require(`express-session`);
+const session = require(`express-session`);
 
 // Sets up the Express App
 // =============================================================
@@ -12,8 +12,26 @@ const PORT = process.env.PORT || 3001;
 const sequelize = require("./config/connection");
 
 // Sets Handlebars as the default template engine
-// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-// app.set("view engine", "handlebars");
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./views");
+
+//Sets route for login page
+app.get("/", (req, res) => {
+  res.render("home", { layout: "main" });
+});
+
+app.get("/login", (req, res) => {
+  res.render("login", { layout: "main" });
+});
+
+app.get("/signup", (req, res) => {
+  res.render("signup", { layout: "main" });
+});
+
+app.get("/dashboard", (req, res) => {
+  res.render("dashboard", { layout: "main" });
+});
 
 // Starts the server to begin listening
 // =============================================================
